fd88f369072835c0580c9bbe19598a3c9a151156
Abhirajkar Bajpai <abhirajkar.bajpai@vyld.io>
2024-09-02 12:21:33 +0530

    initialisation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { GameProvider } from './context/GameContext';
+import GameCanvas from './components/GameCanvas';
+import TetrominoControls from './components/Tetromino';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GameProvider>
+      <div className="App">
+        <GameCanvas />
+        <TetrominoControls />
+      </div>
+    </GameProvider>
   );
 }
 
